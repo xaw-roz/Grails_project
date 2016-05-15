aadd855e8d5a21d0f6b3335b67ed7956737e05c6
xaw-roz <saroj.shrestha@deerwalk.edu.np>
2016-05-15 20:00:39 +0545

    added domain named project

---
+++ Employee_record/grails-app/domain/employee_record/Project.groovy
@@ -0,0 +1,12 @@
+package employee_record
+
+class Project {
+    String projectName
+    String projectId
+    Date startDate
+    Date endDate
+    Float projectBudget
+
+    static constraints = {
+    }
+}
