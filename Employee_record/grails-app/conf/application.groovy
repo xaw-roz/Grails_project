b6ff7bfa89214308e43cc8d8847936d3d3b386c9
xaw-roz <saroj.shrestha@deerwalk.edu.np>
2016-05-27 11:24:05 +0545

    added spring security

---
+++ Employee_record/grails-app/conf/application.groovy
@@ -0,0 +1,33 @@
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'employee_record.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'employee_record.UserRole'
+grails.plugin.springsecurity.authority.className = 'employee_record.Role'
+grails.plugin.springsecurity.logout.postOnly=false
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/error',          access: ['permitAll']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
+	[pattern: '/shutdown',       access: ['permitAll']],
+	[pattern: '/assets/**',      access: ['permitAll']],
+	[pattern: '/**/js/**',       access: ['permitAll']],
+	[pattern: '/**/css/**',      access: ['permitAll']],
+	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+	[pattern: '/project/index',  access: ['permitAll']],
+	[pattern: '/project/create',  access: ['ROLE_ADMIN']],
+	[pattern: '/project/edit/',  access: ['ROLE_ADMIN']],
+	[pattern: '/project/save',  access: ['ROLE_ADMIN']]
+]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+	[pattern: '/assets/**',      filters: 'none'],
+	[pattern: '/**/js/**',       filters: 'none'],
+	[pattern: '/**/css/**',      filters: 'none'],
+	[pattern: '/**/images/**',   filters: 'none'],
+	[pattern: '/**/favicon.ico', filters: 'none'],
+	[pattern: '/**',             filters: 'JOINED_FILTERS']
+]
+
